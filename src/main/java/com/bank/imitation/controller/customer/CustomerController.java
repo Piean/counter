814cf62bbb2c0591cfee58736d101fab5f46d87c
yds <yds4744@163.com>
2016-08-19 02:00:14 +0800

    交易 文件

---
+++ src/main/java/com/bank/imitation/controller/customer/CustomerController.java
@@ -0,0 +1,7 @@
+package com.bank.imitation.controller.customer;
+
+/**
+ * Created by yds on 2016-8-19.
+ */
+public class CustomerController {
+}
--- src/main/java/com/bank/imitation/service/ITradeService.java
+++ src/main/java/com/bank/imitation/service/ITradeService.java
@@ -1,7 +1,21 @@
 package com.bank.imitation.service;
 
+import com.bank.imitation.model.Trade;
+import com.bank.imitation.result.Result;
+
+import java.util.List;
+
 /**
  * Created by mogu on 2016/8/18.
  */
 public interface ITradeService {
+
+    /**
+     * 新增一条交易流水
+     * @param trade
+     * @return
+     */
+    Result<Boolean> insertTrade(Trade trade);
+
+    Result<List<Trade>> queryTrade()
 }
